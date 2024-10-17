15abe36191317d28ccb51e38d54cd542b7f00605
edRibeiro <edersonribeiro.ita@gmail.com>
2024-10-17 12:29:43 -0300

    feat: Retornar palavra com cache da mesma.

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\WordResource;
+use App\Models\Word;
+use Exception;
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+
+class WordController extends Controller
+{
+    function index(): JsonResponse
+    {
+        $wordCursorPaginated = Word::where(function (Builder $query) {
+            if (request()->has('search')) {
+                $query->where('word', 'LIKE', request()->query('search') . "%");
+            }
+        })->orderBy('word')->select('word')->cursorPaginate(request()->query('limit', 15));
+        $responseArray = $wordCursorPaginated->toArray();
+        $responseArray['data'] = Arr::map($responseArray['data'], function (array $item, int $key) {
+            return $item['word'];
+        });
+        return response()->json($responseArray);
+    }
+
+    function show(string $word)
+    {
+        $cacheKey = 'word_' . $word;
+        try {
+            $data = Cache::remember($cacheKey, now()->addMinutes(30), function () use ($word) {
+                return Word::with(['phonetics', 'meanings' => ['definitions' => ['synonyms', 'antonyms'], 'synonyms', 'antonyms'], 'sourceUrls'])->where('word', '=', $word)->firstOrFail();
+            });
+        } catch (Exception $cacheError) {
+            Log::warning('Cache error in Dictionary index: ' . $cacheError->getMessage());
+            try {
+                $data = Word::with(['phonetics', 'meanings' => ['definitions' => ['synonyms', 'antonyms'], 'synonyms', 'antonyms'], 'sourceUrls'])->where('word', '=', $word)->firstOrFail();
+            } catch (Exception $error) {
+                Log::critical('Dictionary index error: ' . $error->getMessage());
+                return response()->json([
+                    'message' => Response::$statusTexts[Response::HTTP_NOT_FOUND],
+                ], Response::HTTP_NOT_FOUND);
+            }
+        }
+        return new WordResource($data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WordController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -21,4 +22,14 @@ Route::group([
     Route::get('/me', [UserController::class, 'me'])->middleware('auth:api')->name('user.me');
     Route::get('/me/history', [UserController::class, 'history'])->middleware('auth:api')->name('user.me.history');
     Route::get('/me/favorites', [UserController::class, 'favorites'])->middleware('auth:api')->name('user.me.favorites');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'entries'
+], function () {
+    Route::prefix('en')->group(function () {
+        Route::get('/', [WordController::class, 'index'])->middleware('auth:api')->name('words.index');
+        Route::get('/{word}', [WordController::class, 'show'])->middleware('auth:api')->name('words.show');
+    });
 });
